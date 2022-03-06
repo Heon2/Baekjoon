37a807d7f6c7c98f61052453427582ee51c96e6a
Heon2 <adlgjs@naver.com>
2022-03-06 23:51:49 +0900

    10757.py

--- 2022.3/2022.03.06/10757.py
+++ 2022.3/2022.03.06/10757.py
@@ -1,6 +1,8 @@
 A, B = map(str, input().split())
-array_A = list(A).reverse()
-array_B = list(B).reverse()
+array_A = list(A)
+array_A.reverse()
+array_B = list(B)
+array_B.reverse()
 result = []
 x = 0  # 오버플로우 초기값
 
@@ -13,7 +15,8 @@ for i in range(max(len(A), len(B))):
     x = sum//10  # 오버플로우
     y = sum % 10  # result에 저장할 수
     result.append(str(y))
-    if(i == max(len(A), len(B)) and x == 1):
+    if(i == max(len(A), len(B))-1 and x == 1):
         result.append('1')
 
-print(''.join(result.reverse()))
+result.reverse()
+print(''.join(result))
